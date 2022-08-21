10190e7dddb2dceea89477ba7d57d2f22c070ec2
Micael Bittencourt <micaelbittencourt.dev@gmail.com>
2022-08-21 14:19:01 -0300

    Criada a estrutura da aplicação e também os Entites.

---
+++ Entities/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Enums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("USR_CPF")]
+        public int CPF { get; set; }
+        [Column("USR_TIPO")]
+        public TipoUsuario? Tipo { get; set; }
+    }
+}
---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notificacoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        [NotMapped]
+        public string mensagem { get; set; }
+        [NotMapped]
+        public List<Notifies> Notificacoes { get; set; }
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    NomePropriedade = nomePropriedade,
+                    mensagem = "Campo Obrigatório",
+                });
+                return false;
+            }
+            return true;
+        }
+        public bool ValidaPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    NomePropriedade = nomePropriedade,
+                    mensagem = "Campo Obrigatório",
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
