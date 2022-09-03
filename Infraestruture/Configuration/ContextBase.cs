ee1e6ecbd44511f1509a0bcf5b79595e05983463
Micael Bittencourt <micaelbittencourt.dev@gmail.com>
2022-09-03 13:27:35 -0300

    Criado a estrutura de Domain e Infrastruture

---
+++ Infraestruture/Configuration/ContextBase.cs
@@ -0,0 +1,37 @@
+﻿
+using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastruture.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "DB_API_DDDD_2022";
+        }
+    }
+}
---
+++ Infraestruture/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastruture.Configuration;
+using Infrastruture.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastruture.Repository.Repositories
+{
+    public class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryMessage()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+    }
+}
